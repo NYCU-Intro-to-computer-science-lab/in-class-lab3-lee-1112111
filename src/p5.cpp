dff442dd5e22d92a57882116a5fb86dc6b7d9b0d
lee-1112111 <chenhenglee1112@gmail.com>
2025-11-25 14:34:38 +0800

    Update p5.cpp

--- src/p5.cpp
+++ src/p5.cpp
@@ -1,11 +1,22 @@
-#include <iostream>
-#include <cmath>
-
+#include <bits/stdc++.h>
 using namespace std;
 
+void hanoi(int n, char from, char aux, char to) {
+    if (n == 1) {
+        cout << "Move disk 1 from " << from << " to " << to << endl;
+        return;
+    }
+
+    hanoi(n-1, from, to, aux);
+
+    cout << "Move disk " << n << " from " << from << " to " << to << endl;
+
+    hanoi(n-1, aux, from, to);
+}
 
 int main() {
-    
-
+    int n;
+    cin >> n;
+    hanoi(n, 'A', 'B', 'C');
     return 0;
 }
