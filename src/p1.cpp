a91945ce3d7404f9aa0365181b1b12b4e0a39cfc
lee-1112111 <chenhenglee1112@gmail.com>
2025-11-25 14:25:00 +0800

    Update p1.cpp

--- src/p1.cpp
+++ src/p1.cpp
@@ -1,10 +1,23 @@
 // 題目 1：高塔建築師 (The Tower Architect) - 20分
 
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
+long long factorial(int n, int t) {
+    if (n == 0) return t;
+    return factorial(n-1, t*=n);
+}
+
+long long sum(int n, int t) {
+    if (n == 0) return t;
+    return sum(n-1, t+=n);
+}
+
 int main() {
+    int x;
+    cin >> x;
     
-    
+    cout << factorial(x, 1) << endl;
+    cout << sum(x, 0) << endl;
     return 0;
 }
